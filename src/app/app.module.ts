e4c55db939ed8199a7c50a21c5d6fdd219ab8bb0
Hansal Bachkaniwala <hansal7014@gmail.com>
2019-09-05 10:24:36 -0400

    Code for news app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,46 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { NewsApiService } from './news-api.service';
 
-import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule, MatCardModule, MatMenuModule, MatToolbarModule, MatIconModule, MatSidenavModule, MatListModule } from '@angular/material';
+
 import { AppComponent } from './app.component';
+import { JwtModule } from "@auth0/angular-jwt";
+import { LoginComponent } from './login/login.component';
+import { NewsFeedComponent } from './news-feed.component';
+
+export function tokenGetter() {
+  return localStorage.getItem('access_token');
+}
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    NewsFeedComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatButtonModule,
+    MatMenuModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatListModule,
+    JwtModule.forRoot({
+      config: {
+        tokenGetter: tokenGetter,
+        authScheme: 'JWT'
+      }
+    })
   ],
-  providers: [],
+  providers: [NewsApiService],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
---
+++ src/app/news-feed.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { NewsApiService } from './news-api.service';
+
+@Component({
+  selector: 'app-news-feed',
+  templateUrl: './news-feed.component.html',
+  styleUrls: ['./news-feed.component.css']
+})
+export class NewsFeedComponent implements OnInit {
+
+  mArticles:Array<any>;
+	mSources:Array<any>;
+	
+	constructor(private newsapi:NewsApiService){
+		console.log('app component constructor called');         
+	}
+
+	ngOnInit() {
+        //load articles
+	    this.newsapi.initArticles().subscribe(data => this.mArticles = data['articles']);
+		//load news sources
+		this.newsapi.initSources().subscribe(data=> this.mSources = data['sources']);	
+    }
+
+
+	searchArticles(source){
+		console.log("selected source is: "+source);
+		this.newsapi.getArticlesByID(source).subscribe(data => this.mArticles = data['articles']);
+	}
+}
